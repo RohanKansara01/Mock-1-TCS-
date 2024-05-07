6219db4fdabe868acd63265d601b63630f0aff8e
RohanKansara01 <rohankansara0110@gmail.com>
2024-05-07 20:01:50 +0530

    routes added and working on home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Quiz from './Pages/Quiz';
+import Home from './Pages/Home';
+import Leaderboard from './Pages/Leaderboard';
+import { ChakraProvider } from '@chakra-ui/react';
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChakraProvider>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/leaderboard' element={<Leaderboard/>}/>
+          <Route path='/quiz' element={<Quiz/>}/>
+        </Routes>
+      </ChakraProvider>
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, Button, Input, Select, Stack } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+     const navigate = useNavigate();
+     const [name ,setName] = useState("");
+     const [category ,setCategory] = useState("");
+     const [level ,setLevel] = useState("");
+     const [number ,setNumber] = useState(3);
+     const handleSubmit=()=>{
+        const user = {
+            name,
+            category,
+            level,
+            number
+        }
+        sessionStorage.setItem("user-data" , JSON.stringify(user));
+        console.log(user)
+        navigate("/quiz")
+      }
   return (
-    <div>
-        
-    </div>
+    <Box>
+      <Input placeholder='Enter Your Name' type='text' value={name} onChange={(e)=>setName(e.target.value)}/>
+      
+      <Stack>
+        <Select placeholder='Select Category' onChange={(e)=>setCategory(e.target.value)}>
+         <option value='General Knowledge'>General Knowledge</option>
+         <option value='Sports'>Sports</option>
+         <option value='Geography'>Geography</option>
+        </Select>
+        <Select placeholder='Select Difficulty' onChange={(e)=>setLevel(e.target.value)}>
+        <option value='Easy'>Easy</option>
+        <option value='Mideum'>Mideum</option>
+        <option value='Hard'>Hard</option>
+        </Select>
+        <Select placeholder='Choose Number Of Questions' onChange={(e)=>setNumber(e.target.value)}>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        </Select>
+      </Stack>
+
+      <Button colorScheme='pink' onClick={handleSubmit} >START QUIZ</Button>
+    </Box>
   )
 }
 
